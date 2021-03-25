f786c6fa8d31ec43f814ab9fa9651bedae9bfb2b
RythmStick <43847240+RythmStick@users.noreply.github.com>
2021-03-25 12:22:40 +0000

    Add files via upload

---
+++ GetCompliant/PasswordHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace GetCompliant
+{
+    public class PasswordHelper
+    {
+        static int compliantCount = 0;
+        static int infileCount = 0;
+        static string password;
+
+        public static void ProcessFile()
+        {
+            Console.WriteLine("[+] Analysing passwords from {0}", Globals.infile);
+            StreamWriter outFile = new StreamWriter(Globals.outfile);
+            StreamReader inFile = new StreamReader(Globals.infile);
+            
+            while ((password = inFile.ReadLine()) != null)
+            {
+               infileCount++;
+               if (iscompliant(password))
+                {
+                    compliantCount++;
+                    outFile.WriteLine(password);
+                }
+            }
+            float percent = compliantCount * 100  / infileCount;
+            Console.WriteLine("[+] Extracted {0} compliant passwords from {1}, hitrate={2}%", compliantCount, infileCount, percent);
+            outFile.Close();
+            inFile.Close();
+         }
+
+        public static Boolean iscompliant(string password)
+        {
+            int passes = 0;
+            if (Globals.minLength>0 && password.Length<Globals.minLength)
+            {
+                return false;
+            }
+            if (Globals.maxLength>0 && password.Length>Globals.maxLength)
+            {
+                return false;
+            }
+            if (Globals.upper && password.ToLower()!=password)
+            {
+                passes++;
+            }
+            if (Globals.lower && password.ToUpper()!=password)
+            {
+                passes++;
+            }
+            if (Globals.number && password.Any(char.IsDigit))
+            {
+                passes++;
+            }
+            if (Globals.special && ContainsSpecial(password))
+            {
+                passes++;
+            }
+            if (passes < Globals.mandatorySets)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool ContainsSpecial(string p)
+        {
+            foreach (var c in p)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ GetCompliant/Program.cs
@@ -0,0 +1,138 @@
+﻿using NDesk.Options;
+using System;
+using System.IO;
+
+namespace GetCompliant
+{
+    using static Globals;
+    public static class Globals
+    {
+        public static string infile;
+        public static string outfile;
+        public static int minLength=0;
+        public static int maxLength=0;
+        public static Boolean force;
+        public static Boolean upper;
+        public static Boolean lower;
+        public static Boolean number;
+        public static Boolean special;
+        public static int mandatorySets=0;
+        public static Boolean help;
+        public static string setsverbose;
+        public static int setsSpecified = 0;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!validParameters(args))
+            {
+                return;
+            }
+            SummarizePolicy();
+            PasswordHelper.ProcessFile();
+        }
+
+        public static void SummarizePolicy()
+        {
+            Console.WriteLine("[+] Extracting passwords that meet the following complexity rules:");
+            if (setsSpecified > 0)
+            {
+                Console.WriteLine("    Must include {0} of the following sets: {1}", mandatorySets, setsverbose);
+            }
+            if (minLength > 0)
+            {
+                Console.WriteLine("    Minimum Length is {0}", minLength);
+            }
+            if (maxLength > 0)
+            {
+                Console.WriteLine("    Maximum Length is {0}", maxLength);
+            }
+        }
+
+        public static Boolean validParameters(string[] args)
+        {
+            var options = new OptionSet(){
+                {"i|inputfile=", "Password File eg. rockyou.txt", o => infile = o},
+                {"o|outputfile=", "Output File", o => outfile = o},
+                {"f|forceoverwrite","force overwrite if Output file exists", o => force = true},
+                {"m|minlength=", "Minimum password Length", (int o) => minLength = o},
+                {"x|maxlength=", "Maximum password Length", (int o) => maxLength = o},
+                {"u|upper","must include Uppercase set", o => upper = true},
+                {"l|lower","must include Lowercase set", o => lower = true},
+                {"p|special","must include Special Char set", o => special = true},
+                {"n|number","must include Number set", o => number = true},
+                {"s|sets=","number of Mandatory sets (default: All Sets)", (int o) => mandatorySets = o},
+                {"h|?|help","Show Help", o => help = true},
+            };
+            try
+            {
+                options.Parse(args);
+                if (help || args.Length == 0)
+                {
+                    showHelp(options);
+                    return false;
+                }
+                if (infile==null)
+                {
+                    Console.WriteLine("[-] Must specify an input file using -i");
+                    return false;
+                }
+                if (!File.Exists(infile))
+                {
+                    Console.WriteLine("[-] File {0} not found", infile);
+                    return false;
+                }
+                if (outfile == null)
+                {
+                    Console.WriteLine("[-] Must specify an output file using -o");
+                    return false;
+                }
+                if ((!File.Exists(outfile)))
+                {
+                    FileStream fs = File.Create(outfile); 
+                    fs.Close();
+                }
+                else if (!force)
+                {
+                    Console.WriteLine("[-] Output File already exists, use -f to force overwrite");
+                    return false;
+                }
+                if (upper)  { setsSpecified++; setsverbose += "Uppercase "; }
+                if (lower)  { setsSpecified++; setsverbose += "Lowercase "; }
+                if (special) { setsSpecified++; setsverbose += "Special "; }
+                if (number) { setsSpecified++; setsverbose += "Numbers "; } 
+                if (mandatorySets > setsSpecified)
+                {
+                    Console.WriteLine("[-] Mandatory Sets -s ({0}) must not be > sets supplied ({1})", mandatorySets, setsSpecified);
+                    return false;
+                }
+                if (mandatorySets==0)
+                {
+                    mandatorySets = setsSpecified;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                showHelp(options);
+                return false;
+            }
+            return true;
+        }
+
+        public static void showHelp(OptionSet p)
+        {
+            Console.WriteLine(@"           _                          _  _            _   ");
+            Console.WriteLine(@" ___  ___ | |_  ___  ___  _____  ___ | ||_| ___  ___ | |_");
+            Console.WriteLine(@"| . || -_||  _||  _|| . ||     || . || || || .'||   ||  _|");
+            Console.WriteLine(@"| _ ||___||_|  |___||___||_|_|_||  _||_||_||__,||_|_||_|");
+            Console.WriteLine(@"|___|                           |_|                       ");
+            Console.WriteLine("@_RythmStick\n\n\n");
+            Console.WriteLine("Compliant password Extractor\nUsage:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
+
